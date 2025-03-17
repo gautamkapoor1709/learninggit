4d70d1ff9bc436cca2c3fd6b0d0a8ae7180cb296
Gautam <gautamkapoor1709@gmail.com>
2025-03-17 17:47:10 +0530

    Still Trying

---
+++ A.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    coutt<<"Hello"<<endl;
+    return 0;
+    jdsakjsa'
+    int a ='a';
+    reallocarray
+    
+}
---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+usinf namespace std;
+
+int main(){
+    cout<<kello;
+    "hello my fri";
+    cin<<ass;
+    assa=2332;
+    sdds==122112;
+    alloca
+    cout<<alloca<<endl
+    return 200000000
+    
+}
